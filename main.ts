ef20551e67202248aefe4f87fda90d1dd0467808
garfbit <104676660+garfbit@users.noreply.github.com>
2025-08-29 06:57:09 -0500

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,7 @@
-
+let voltaje = 0
+let temperatura = 0
+basic.forever(function () {
+    voltaje = Math.map(pins.analogReadPin(AnalogPin.P0), 0, 1023, 0, 3300)
+    temperatura = voltaje - Math.idiv(500, 10)
+    basic.showNumber(temperatura)
+})
